bf4994872f4e79f34701ab6834c96c1ba6bfee55
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-25 22:32:56 +0300

    Card changes

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 const WeatherCard = (props) => {
 return(
     <div >
 
       <Card style={{ borderRadius: '1rem', height: '20rem' }} className= 'mt-3'>
-        <Card.Img variant="top" src="" />
+        <Card.Img variant="top" src="" style={{width: 'auto', height: '5rem'}} />
         <Card.Body>
           <Card.Title>City: </Card.Title>
-          <Card.Text><p>Temp:</p></Card.Text>
-          <Card.Text><p>WeatherText:</p></Card.Text>
+          <Card.Text><strong>Temp:</strong></Card.Text>
+          <Card.Text><strong>WeatherText:</strong></Card.Text>
           
         </Card.Body>
       </Card>
