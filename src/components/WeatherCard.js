8442c6bb1534e98ac66812f7b8ce0030f3c19c11
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-27 09:14:03 +0300

    .env file removed from gitignore

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -11,7 +11,7 @@ const WeatherCard = ({ cityData }) => {
 
   useEffect(() => {
     const apiKey = process.env.REACT_APP_API
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${cityData.Key}?apikey=${apiKey}`
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/${cityData.Key}?apikey=ICfOrVGI3ofdnGODMlLrRMwyPbISOCdO`
 
     axios.get(url)
       .then((res) => {
