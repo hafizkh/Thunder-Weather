59310d490403db8c76b48258ea259d3d2c26ea11
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-27 05:02:39 +0300

    Images added

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card } from "react-bootstrap";
 import bg from '../images/bg_pic.jpg'
+import night from '../images/night_bg.jpg'
 
 
 const WeatherCard = ({ cityData }) => {
@@ -23,10 +24,11 @@ const WeatherCard = ({ cityData }) => {
     {data &&(
 
       <Card className= 'mt-3'>
-        <Card.Img variant="top" src={bg} style={{width: 'auto', height: 'auto'}} />
+        {data.IsDayTime===true?<Card.Img variant="top" src={bg} style={{width: 'auto', height: 'auto'}} />
+        :<Card.Img variant="top" src={night} style={{width: 'auto', height: 'auto'}} />}
         <Card.Body style={{border: '2px solid'}}>
           <Card.Title style={{fontSize: 'xx-large'}}>{cityData.EnglishName}, {cityData.Country.EnglishName} </Card.Title>
-          <Card.Text style={{fontSize: 'xx-large'}} ><strong>{Math.ceil(data.Temperature.Metric.Value)} °C</strong></Card.Text>
+          <Card.Text style={{fontSize: 'xx-large'}} ><strong>Temp: {Math.ceil(data.Temperature.Metric.Value)} °C</strong></Card.Text>
           <Card.Text style={{fontSize: 'x-large'}}><strong>{data.WeatherText}</strong></Card.Text>
           
         </Card.Body>
