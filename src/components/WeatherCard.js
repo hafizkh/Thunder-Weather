1f01db015be935522cf9837f10f2e8d78a4732ab
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-30 00:25:17 +0300

    some updates

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Card } from "react-bootstrap";
 import bg from '../images/bg_pic.jpg'
 import night from '../images/night_bg.jpg'
-import component_bg from '../images/component_bg.jpg'
 
 
 
@@ -26,10 +25,10 @@ const WeatherCard = ({ cityData }) => {
     <>
     {value &&(
 
-      <Card className= 'mt-3'>
+      <Card className= 'mt-3' style={{border: '2px solid'}}>
         {value.IsDayTime===true?<Card.Img variant="top" src={bg} style={{width: 'auto', height: 'auto'}} />
         :<Card.Img variant="top" src={night} style={{width: 'auto', height: 'auto'}} />}
-        <Card.Body style={{border: '2px solid'}}>
+        <Card.Body >
           <Card.Title style={{fontSize: 'xx-large'}}>{cityData.EnglishName}, {cityData.Country.EnglishName} </Card.Title>
           <Card.Text style={{fontSize: 'xx-large'}} ><strong>Temp: {Math.ceil(value.Temperature.Metric.Value)} °C</strong></Card.Text>
           <Card.Text style={{fontSize: 'x-large'}}><strong>{value.WeatherText}</strong></Card.Text>
