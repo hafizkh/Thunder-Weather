01d80a53ec2948f53f743caf5f34add5b7281bd8
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-03-06 01:20:55 +0200

    changes in about

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,68 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
-import NavBar from './components/NavBar'
-import Weather from './components/Weather'
-import WeatherCard from './components/WeatherCard';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Footer from './components/Footer';
-
+import React, { useState } from "react";
+import axios from "axios";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import NavBar from "./components/NavBar";
+import Weather from "./components/Weather";
+import WeatherCard from "./components/WeatherCard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [cityData, setCityData] = useState(null);
 
-  const [search, setSearch] = useState('');
-  const [cityData, setCityData]=useState(null);
-
-const apiKey = process.env.REACT_APP_API
-  const getCityInfo = (event) =>{
+  const apiKey = process.env.REACT_APP_API;
+  const getCityInfo = (event) => {
     event.preventDefault();
-    axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${search}`)
-    .then((res)=>{
-      setCityData(res.data[0]);
-      setSearch('');
-    }).catch(e=>alert(e));
-  }
+    axios
+      .get(
+        `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${search}`
+      )
+      .then((res) => {
+        setCityData(res.data[0]);
+        setSearch("");
+      })
+      .catch((e) => alert(e));
+  };
 
   return (
     <>
-    <div className='App' >
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/home' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/weather' element={<Weather cityData= {cityData} search={search}
-        setSearch={setSearch} getCityInfo={getCityInfo} />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/weatherCard' element={<WeatherCard cityData={cityData} setCityData={setCityData} />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-<Footer/>
-    </Router>
-    
-  </div>
-  </>
-
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/weather"
+              element={
+                <Weather
+                  cityData={cityData}
+                  search={search}
+                  setSearch={setSearch}
+                  getCityInfo={getCityInfo}
+                />
+              }
+            />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route
+              exact
+              path="/weatherCard"
+              element={
+                <WeatherCard cityData={cityData} setCityData={setCityData} />
+              }
+            />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </>
   );
 }
 
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Form, Button } from "react-bootstrap"
-import WeatherCard from './WeatherCard'
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import WeatherCard from "./WeatherCard";
 
-const Weather = ({search,setSearch, getCityInfo, cityData}) => {
-  const handleChange =(e)=>{
-    setSearch(e.target.value)
-  }
+const Weather = ({ search, setSearch, getCityInfo, cityData }) => {
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
-      <div style={{width: '25rem'}} className='container'>
-      <h2 className='mt-2'>Thunder Weather Client</h2>
+    <div style={{ width: "25rem" }} className="container">
+      <h2 className="mt-2">Thunder Weather Client</h2>
 
-      <Form style={{ height: '2.4rem'}} className="d-flex mt-2" onSubmit={getCityInfo}>
+      <Form
+        style={{ height: "2.4rem" }}
+        className="d-flex mt-2"
+        onSubmit={getCityInfo}
+      >
         <Form.Control
           type="search"
           placeholder="Search Weather By City"
@@ -20,13 +24,17 @@ const Weather = ({search,setSearch, getCityInfo, cityData}) => {
           value={search}
           onChange={handleChange}
         />
-        <Button className="btn-success" onClick={getCityInfo}>Search</Button>
+        <Button className="btn-success" onClick={getCityInfo}>
+          Search
+        </Button>
       </Form>
-      {cityData && <div ><WeatherCard cityData={cityData}/></div>}
-
+      {cityData && (
+        <div>
+          <WeatherCard cityData={cityData} />
+        </div>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
