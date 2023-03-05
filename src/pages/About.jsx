e9f76bbc13db866537173a374934938579107e72
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-03-06 01:07:03 +0200

    changes in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import about from '../images/about.jpg'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import about from "../images/about.jpg";
 
 const About = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-      <div className='mt-2'>
-      <img src= {about} style={{height: "auto", width: "49rem"}} alt="About me" />
-      <div className="card-body">
-        <button type='button' 
-        className="btn btn-primary btn-rounded" 
-        onClick={()=>navigate("/home")}>Home</button>
+    <>
+      <h1 className="mt-3">Hafiz's Persona</h1>
+      <div>
+        <img
+          src={about}
+          style={{ height: "auto", width: "45rem" }}
+          alt="About me"
+        />
+        <div className="card-body">
+          <button
+            type="button"
+            className="btn btn-primary btn-rounded"
+            onClick={() => navigate("/home")}
+          >
+            Home
+          </button>
+        </div>
       </div>
-    </div>
-    )
-  }
+    </>
+  );
+};
 
-export default About
+export default About;
