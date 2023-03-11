64110022fdc67e763f91348f93ffacf6a47f5aaa
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-03-12 00:41:41 +0200

    updated

--- src/pages/ThunderWeather.jsx
+++ src/pages/ThunderWeather.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ThunderWeather = () => {
   return (
+    <div>
+       
     <h1>Thunder-Weather</h1>
+    <h3>Please return to the Weather Card</h3>
+    </div>
   )
 }
 
